c1e5940e775508b7650dc33c57e993ffea9a6340
Filip Jirsák <filip@jirsak.org>
2024-04-08 15:37:32 +0200

    fix formatting

--- src/main/java/cz/czechitas/java2webapps/lekce1/Application.java
+++ src/main/java/cz/czechitas/java2webapps/lekce1/Application.java
@@ -11,11 +11,11 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 public class Application {
-	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String... args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-		logger.info("Aplikace běží na adrese: http://localhost:{}", applicationContext.getEnvironment().getProperty("local.server.port"));
-	}
+    public static void main(String... args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        logger.info("Aplikace běží na adrese: http://localhost:{}", applicationContext.getEnvironment().getProperty("local.server.port"));
+    }
 
 }
